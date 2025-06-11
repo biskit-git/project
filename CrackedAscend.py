8d439d7a03979bfa4d7e028c579289088266c1fe
biskit-git <levgoolie@gmail.com>
2025-06-11 12:36:25 +1000

    Update CrackedAscend.py

--- CrackedAscend.py
+++ CrackedAscend.py
@@ -117,7 +117,7 @@ def center_text (OO0OO00OO0000OOO0 ):#line:113
 def load_settings ():#line:117
     os .system ("cls")#line:118
     print (f"{LIGHT_WHITE}Pick {LIGHT_PURPLE}Default {LIGHT_WHITE}[{LIGHT_CYAN}1{LIGHT_WHITE}] ")#line:119
-    print (f"{LIGHT_WHITE}Pick {LIGHT_PURPLE}original jitter {LIGHT_WHITE}[{LIGHT_CYAN}2{LIGHT_WHITE}]")#line:120
+    print (f"{LIGHT_WHITE}Pick {LIGHT_PURPLE}original jitter(Jitter May Be Stronger Then Option {LIGHT_WHITE}[1]\033[0m {LIGHT_WHITE}[{LIGHT_CYAN}2{LIGHT_WHITE}]")#line:120
     O00O00OO0OOO0OO00 =input (f"{LIGHT_WHITE}What Jitter Setting, {LIGHT_PURPLE}Do You Want To Use{LIGHT_PURPLE}:{LIGHT_CYAN}= {LIGHT_CYAN}")#line:121
     if O00O00OO0OOO0OO00 =="1":#line:122
        return (24 ,24 ,24 ,28 ,0.013 )#line:123
